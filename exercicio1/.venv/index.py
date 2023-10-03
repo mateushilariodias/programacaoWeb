b76d2eb4a481b04a6b1c0d07cde1154c80c86701
mateushilariodias <mateusilariodias@gmail.com>
2023-10-03 20:38:17 -0300

    change exercicio2

--- exercicio1/.venv/index.py
+++ exercicio1/.venv/index.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template;
+from flask import Flask, render_template, request;
 
 app = Flask(__name__);
 
 @app.route('/')
 def index():
-    return render_template('principal.html', titulo="Marketplace")
+    return render_template('login.html', titulo="Marketplace")
 
-@app.route('/login')
-def indexTwo():
-    return render_template("<h1>Seu e-mail:</h1>", email="mateusilariodias@gmail.com")
+@app.route('/login', methods=['POST'])
+def login():
+    login_user = request.form['login']
+    passworld_user = request.form['passworld']
 
-@app.route('/clientes')
-def indexThree():
-    return "Aline"\
-            "Julio"
+    if login_user == 'admin' and passworld_user == '123':
+        return 'logou'
+    else:
+        return render_template('principal.html', titulo="Usuário ou senha inválido")
 
 app.run();
